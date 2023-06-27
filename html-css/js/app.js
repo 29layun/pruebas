361b851a522c3cce2632b7997cb5f03f13b5307c
29layun <mjimenezt003@alumno.uaemex.mx>
2023-06-27 13:30:10 -0600

    Agregando fondo animado

---
+++ html-css/js/app.js
@@ -0,0 +1,112 @@
+particlesJS(
+    {
+        "particles": {
+          "number": {
+            "value": 19,
+            "density": {
+              "enable": true,
+              "value_area": 881.8766334760375
+            }
+          },
+          "color": {
+            "value": "#0c0c3f"
+          },
+          "shape": {
+            "type": "edge",
+            "stroke": {
+              "width": 2,
+              "color": "#9595df"
+            },
+            "polygon": {
+              "nb_sides": 5
+            },
+            "image": {
+              "src": "img/github.svg",
+              "width": 100,
+              "height": 100
+            }
+          },
+          "opacity": {
+            "value": 0.38481889460772545,
+            "random": false,
+            "anim": {
+              "enable": false,
+              "speed": 1,
+              "opacity_min": 0.1,
+              "sync": false
+            }
+          },
+          "size": {
+            "value": 90.75197878771135,
+            "random": true,
+            "anim": {
+              "enable": false,
+              "speed": 40,
+              "size_min": 0.1,
+              "sync": false
+            }
+          },
+          "line_linked": {
+            "enable": false,
+            "distance": 426.1397264814272,
+            "color": "#fffff",
+            "opacity": 0.4,
+            "width": 1
+          },
+          "move": {
+            "enable": true,
+            "speed": 9.22388442605866,
+            "direction": "bottom-right",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+              "enable": false,
+              "rotateX": 1042.21783956259,
+              "rotateY": 801.7060304327614
+            }
+          }
+        },
+        "interactivity": {
+          "detect_on": "canvas",
+          "events": {
+            "onhover": {
+              "enable": true,
+              "mode": "bubble"
+            },
+            "onclick": {
+              "enable": true,
+              "mode": "repulse"
+            },
+            "resize": true
+          },
+          "modes": {
+            "grab": {
+              "distance": 107.8921078921079,
+              "line_linked": {
+                "opacity": 1
+              }
+            },
+            "bubble": {
+              "distance": 401.97822008916586,
+              "size": 40,
+              "duration": 2,
+              "opacity": 8,
+              "speed": 3
+            },
+            "repulse": {
+              "distance": 200,
+              "duration": 0.2
+            },
+            "push": {
+              "particles_nb": 4
+            },
+            "remove": {
+              "particles_nb": 2
+            }
+          }
+        },
+        "retina_detect": false
+      }
+);
